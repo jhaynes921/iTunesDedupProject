447d19a8db1c1b63672764e5ac03c3018004b0a8
John Haynes <JohnHaynes@jmhastronomy.com>
2026-08-09 20:09:29 -0400

    working

--- iTunesDeDuplicationProject/iTunesDeDuplicationProject/frmMain.cs
+++ iTunesDeDuplicationProject/iTunesDeDuplicationProject/frmMain.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Documents.Serialization;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace iTunesDeDuplicationProject
 {
@@ -17,20 +18,23 @@ namespace iTunesDeDuplicationProject
 	public partial class frmMain : Form
 	{
 
-		#region Setup Stuff
+        #region Setup Stuff
 
-		internal string testLocation = "J:\\Toshiba_Backup\\iTunes_Backup\\iTunes Media\\Music\\Journey\\_backups";
+        internal string testLocation = "J:\\Toshiba_Backup\\iTunes_Backup\\iTunes Media\\Music\\Journey\\_backups";
 
+		/*
 		string[] AudioFileTypes = new string[] { ".mp3", ".m4a", ".m4b", ".m4p", ".m4r", ".wav", ".flac", ".aac", ".wma", ".wav" };
 		string[] VideoFileTypes = new string[] { ".mp4", ".m4v", ".mov", ".avi", ".wmv", ".flv", ".mkv", ".mpg", ".asf", ".webm" };
 		string[] PictureFileTypes = new string[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg" };
+        */
+
+		HashSet<string> AudioFileTypes = new HashSet<string>( new[] { ".txt", ".csv", ".xml", ".json" }, StringComparer.OrdinalIgnoreCase);
 
 		public frmMain()
 		{
 			InitializeComponent();
 			tbDirPath.Text = testLocation;
 		}
-
 
 
 		public void WriteToDisplay(string MessageText, bool ClearFirst = false, bool SuppressTimeStamps = false)
@@ -55,71 +59,38 @@ namespace iTunesDeDuplicationProject
 			MainProcess(tbDirPath.Text);
 		}
 
-#endregion
+		#endregion Setup Stuff
 
 
-		public void MainProcess(string DirName) 
+        public void MainProcess(string DirName) 
 		{
-			SearchOption option = SearchOption.TopDirectoryOnly;
-			if (!Directory.Exists(DirName))
-			{
-				WriteToDisplay("Directory \"" + DirName + "\" is invalid or does not exist.", true, false);
-				return;
-			}
-			else
-			{
-				WriteToDisplay("Now processing directory \"" + DirName + "\"", true, false);
-				if (tglDirOption.Checked)
-				{
-					option = SearchOption.AllDirectories;
-					WriteToDisplay("Searching all subdirectories.", false, false);
-				}
-				else
-				{
-					option = SearchOption.TopDirectoryOnly;
-					WriteToDisplay("Searching this directory only.", false, false);
-				}
-			}
+            List<string> fileList = GetFileList(DirName);
 
-
-			string[] allFiles = Directory.GetFiles(DirName,"*.*", option);
-
-			int fileCount = allFiles.Length;
-			WriteToDisplay("Files found in directory: " + fileCount.ToString(), false, false);
-			WriteToDisplay("Getting list of extensions...", false, false);
-
-			List<string>extensionList = new List<string>();
-			
-			foreach (string file in allFiles)
-			{ 
-				FileInfo fi = new FileInfo(file);
-				string ext = fi.Extension.ToLower();
-
-				if (!extensionList.Contains(ext))
-				{
-					extensionList.Add(ext);
-				}
-			}
-
-			StringBuilder sb = new StringBuilder();
-			foreach(string ext in extensionList)
-			{
-				sb.AppendLine(ext);
-			}
-
-			if(sb.Length > 0)
-			{
-				WriteToDisplay("File extensions found in directory:\r\n" + sb.ToString(), false, false);
-			}
-			else
-			{
-				WriteToDisplay("No files found in directory.", false, false);
-			}
-
+            int fileCount = fileList.Count;
+            WriteToDisplay("Total files found: " + fileCount.ToString("N0"));
 
 		}
 
-		private void tglDirOption_CheckedChanged(object sender, EventArgs e)
+
+        public List<string>GetFileList(string DirName)
+        {
+            List<string>fileList = new List<string>();
+
+            SearchOption srchOption = SearchOption.TopDirectoryOnly;
+            if (tglDirOption.Checked) { srchOption = SearchOption.AllDirectories; }
+			fileList = Directory.EnumerateFiles(DirName, "*.*", srchOption).Where(s => AudioFileTypes.Contains(Path.GetExtension(s).ToLower())).ToList<string>();
+            return fileList;
+           
+        }
+
+
+
+
+
+
+        #region Control Interactions
+
+        private void tglDirOption_CheckedChanged(object sender, EventArgs e)
 		{
 			if(tglDirOption.Checked)				{
 				lbDirOption.Text = "This directory and all subdirectories.";
@@ -129,5 +100,72 @@ namespace iTunesDeDuplicationProject
 				lbDirOption.Text = "This directory only";
 			}
 		}
-	}
+
+        #endregion Control Interactions
+
+        #region Holding Bin
+
+        public void MainProcess_HOLDING(string DirName)
+        {
+            SearchOption option = SearchOption.TopDirectoryOnly;
+            if (!Directory.Exists(DirName))
+            {
+                WriteToDisplay("Directory \"" + DirName + "\" is invalid or does not exist.", true, false);
+                return;
+            }
+            else
+            {
+                WriteToDisplay("Now processing directory \"" + DirName + "\"", true, false);
+                if (tglDirOption.Checked)
+                {
+                    option = SearchOption.AllDirectories;
+                    WriteToDisplay("Searching all subdirectories.", false, false);
+                }
+                else
+                {
+                    option = SearchOption.TopDirectoryOnly;
+                    WriteToDisplay("Searching this directory only.", false, false);
+                }
+            }
+
+
+            string[] allFiles = Directory.GetFiles(DirName, "*.*", option);
+
+            int fileCount = allFiles.Length;
+            WriteToDisplay("Files found in directory: " + fileCount.ToString(), false, false);
+            WriteToDisplay("Getting list of extensions...", false, false);
+
+            List<string> extensionList = new List<string>();
+
+            foreach (string file in allFiles)
+            {
+                FileInfo fi = new FileInfo(file);
+                string ext = fi.Extension.ToLower();
+
+                if (!extensionList.Contains(ext))
+                {
+                    extensionList.Add(ext);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string ext in extensionList)
+            {
+                sb.AppendLine(ext);
+            }
+
+            if (sb.Length > 0)
+            {
+                WriteToDisplay("File extensions found in directory:\r\n" + sb.ToString(), false, false);
+            }
+            else
+            {
+                WriteToDisplay("No files found in directory.", false, false);
+            }
+
+        }
+
+        #endregion Holding Bin
+
+    }
 }
